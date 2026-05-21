450fd2735d77ecef656c3f0af32b7a125da1027a
FabianSeemannBiF <if25b223@technikum-wien.at>
2026-05-21 13:18:09 +0200

    implement simplest production code to pass two comma-separated numbers

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -4,5 +4,11 @@ StringCalculator.prototype.add = function(string_numbers) {
 	if(string_numbers === "") {
         return 0;
     }
+
+    if (string_numbers.indexOf(',') !== -1) {
+        const parts = string_numbers.split(',');
+        return parseInt(parts[0]) + parseInt(parts[1]);
+    }
+
     return parseInt(string_numbers);
 };
