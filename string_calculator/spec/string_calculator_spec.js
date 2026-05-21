ff8d2ceab8e69b5f3fe09c61a60f4468772697d2
FabianSeemannBiF <if25b223@technikum-wien.at>
2026-05-21 13:27:27 +0200

    write test for two comma-separated numbers

--- string_calculator/spec/string_calculator_spec.js
+++ string_calculator/spec/string_calculator_spec.js
@@ -11,7 +11,11 @@ describe('String Calculator', function() {
 		expect(calculator.add('1')).toEqual(1);
 	})
 
-	it('should return 3 when inout is "1, 2"', function() {
+	it('should return 3 when input is "1, 2"', function() {
 		expect(calculator.add('1, 2')).toEqual(3);
+	})
+
+	it('should return 28 when input is "1, 27"', function() {
+		expect(calculator.add('1, 27')).toEqual(28);
 	})
 });
